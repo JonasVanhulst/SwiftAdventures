01a8e9a8730b2bae754153ed10f3c14e4a757db4
Jonas <114076210+JonasVanhulst@users.noreply.github.com>
2024-02-16 22:46:55 +0100

    Second day programming 5h.swift

--- learnings.swift
+++ learnings.swift
@@ -132,7 +132,134 @@ func range_check(first_digit : Int , second_digit : Int) -> Bool {
     }
 }
 
-var greeting = "swift, playground"
+// Write a Swift program to check if a given string begins with "fix", except the 'f' can be any character or number.
+func find_character(word : String) -> Bool {
+    let count = word.count
+    
+    if count < 1 {
+        return false
+    }
+    
+    var new_word = word
+    let corrected_word = new_word.removeFirst()
+    
+    if new_word.hasPrefix("ix") {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+// Write a Swift program to find the largest number among three given integers.
+func find_largest(first_number : Int, second_number : Int, third_number : Int) -> Int {
+    if first_number > second_number {
+        if first_number > third_number {
+            return first_number
+        } else {
+            return third_number
+        }
+    } else {
+        if second_number > third_number {
+            return second_number
+        } else {
+            return third_number
+        }
+    }
+}
+
+// Write a Swift program that accept two integer values and to test which value is nearest to the value 10, or return 0 if both integers have same distance from 10.
+func check_nearest(first_number : Int, second_number : Int) -> Int {
+    var subresult_one = 0
+    var subresult_two = 0
+    
+    if first_number > 10 {
+        subresult_one = first_number - 10
+    } else {
+        subresult_one = 10 - first_number
+    } 
+    
+    if second_number > 10 {
+        subresult_two = second_number - 10
+    } else {
+        subresult_two = 10 - second_number
+    }
+    
+    if subresult_one == subresult_two {
+        return 0
+    } else if subresult_one < subresult_two {
+        return first_number
+    } else {
+        return second_number
+    }
+}
+
+// Write a Swift program that accept two integer values and test if they are both in the range 20..30 inclusive, or they are both in the range 30..40 inclusive.
+func check_20_40_range(first_number : Int, second_number : Int) -> Bool {
+    if (first_number >= 20 && second_number >= 20) && (first_number <= 30 && second_number <= 30) {
+        return true
+    } else if (first_number >= 30 && second_number >= 30) && (first_number <= 40 && second_number <= 40) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// Write a Swift program that accept two positive integer values and test whether the larger value is in the range 20..30 inclusive, or return 0 if neither is in that range.
+func check_20_30_range(first_number: Int, second_number: Int) -> Int {
+    if (first_number >= 20 && first_number <= 30) || (second_number >= 20 && second_number <= 30) {
+        return max(first_number, second_number)
+    } else {
+        return 0
+    }
+}
+
+// Write a Swift program to test whether the last digit of the two given non-negative integer values are same or not.
+func areLastDigitsSame(firstNumber: Int, secondNumber: Int) -> Bool {
+    guard firstNumber >= 0 && secondNumber >= 0 else {
+        return false
+    }
+    
+    let lastDigitFirst = firstNumber % 10
+    let lastDigitSecond = secondNumber % 10
+
+    return lastDigitFirst == lastDigitSecond
+}
+
+// Write a Swift program to convert the last three characters in upper case. If the string has less than 3 chars, lowercase whatever is there.
+func string_converter(sentence: String) -> String {
+    var newSentence = ""
+    let characterCount = sentence.count
+    
+    if characterCount < 3 {
+        return sentence.lowercased()
+    } else {
+        let indexToStartUppercase = sentence.index(sentence.endIndex, offsetBy: -3)
+        let prefix = sentence.prefix(upTo: indexToStartUppercase)
+        let suffix = sentence.suffix(from: indexToStartUppercase).uppercased()
+        newSentence = String(prefix + suffix)
+        return newSentence
+    }
+}
+
+// Write a Swift program to check if the first instance of "a" in a given string is immediately followed by another "a".
+func first_instance_check(sentence : String) -> Bool {
+    if let firstAIndex = sentence.firstIndex(of: "a") {
+        let nextIndex = sentence.index(after: firstAIndex)
+        
+        if nextIndex < sentence.endIndex && sentence[nextIndex] == "a" {
+            return true
+        } else {
+            return false
+        }
+    } else {
+        return false
+    }
+}
+
+
+
+var tutorial = "swift, playground"
 
 print(summing(firts_digit: 55, second_digit: 5))
 print(compute(digit: 45))
@@ -146,7 +273,11 @@ print(non_negative_digit_check(number: 33))
 print(two_digit_adder(sentence: "Apple"))
 print(is_string_check(sentence: "I s appel "))
 print(range_check(first_digit: 15, second_digit: 40))
-
-
-
-
+print(find_character(word: "fix gold"))
+print(find_largest(first_number: 200, second_number: 2, third_number: 130))
+print(check_nearest(first_number: 8, second_number: 13))
+print(check_20_40_range(first_number: 20, second_number: 30))
+print(check_20_30_range(first_number: 22, second_number: 29))
+print(areLastDigitsSame(firstNumber: 3, secondNumber: 13))
+print(string_converter(sentence: "hi"))
+print(first_instance_check(sentence: "abbcaad"))
